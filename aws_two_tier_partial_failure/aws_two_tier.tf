5d1af3ec29ee7ab653bc6fb499a95f232e5be380
Jagadeesan Murugesan <jmurugesan@sinmackjemd6m.paloaltonetworks.local>
2020-12-04 16:24:04 +0530

    Modified files:[aws_two_tier.tf bad_file.tf]

--- aws_two_tier_partial_failure/aws_two_tier.tf
+++ aws_two_tier_partial_failure/aws_two_tier.tf
@@ -316,3 +316,5 @@ output "FirewallManagementURL" {
 output "WebURL" {
   value = "${join("", list("http://", "${aws_eip.PublicElasticIP.public_ip}"))}"
 }
+
+# Modified [aws_two_tier.tf] on [2020-12-04 16:24:04.808977]
