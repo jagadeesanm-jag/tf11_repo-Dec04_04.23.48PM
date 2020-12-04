06b1348958bf6e8815d4c844c34e4dc222f8a0de
Jagadeesan Murugesan <jmurugesan@sinmackjemd6m.paloaltonetworks.local>
2020-12-04 16:24:35 +0530

    Modified files:[aws_two_tier.tf bad_file.tf bad_file.tf good.tf]

--- aws_two_tier_files_and_folders_specified/good_and_bad_files/good.tf
+++ aws_two_tier_files_and_folders_specified/good_and_bad_files/good.tf
@@ -316,3 +316,5 @@ output "FirewallManagementURL" {
 output "WebURL" {
   value = "${join("", list("http://", "${aws_eip.PublicElasticIP.public_ip}"))}"
 }
+
+# Modified [good.tf] on [2020-12-04 16:24:35.786402]
